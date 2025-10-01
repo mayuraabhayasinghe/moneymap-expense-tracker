0461b6419fdd48085a8faa534d5b6c8b1b07d150
mayuraabhayasinghe <abhayasinghemayura@gmail.com>
2025-10-02 02:11:44 +0530

    authentication fornt-end is complete

---
+++ frontend/moneymap-expense-tracker/src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expenses from "./pages/Dashboard/Expenses";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expenses" element={<Expenses />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
+
+const Root = () => {
+  //Check the existance of local storage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  //Redirect to dashboard if authenticated otherwise to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
