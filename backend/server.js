cff01cf6301ea3842fa0743d3676843ca066107d
mayuraabhayasinghe <abhayasinghemayura@gmail.com>
2025-10-03 03:56:53 +0530

    Developed authentication APIs

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+require("dotenv").config();
 
 const app = express();
 
 //Middleware for cors handling
 app.use(
-  cosr({
+  cors({
     origin: process.env.CLIENT_URL || "*",
     method: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["content-Type", "Authorization"],
@@ -15,6 +18,13 @@ app.use(
 );
 
 app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
