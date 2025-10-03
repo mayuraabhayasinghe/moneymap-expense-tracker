855ef78edfc9c5562b6ff572990ab2cf5d3c3740
mayuraabhayasinghe <abhayasinghemayura@gmail.com>
2025-10-03 21:40:52 +0530

    created other all other APIs.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/getUser", protect, getUserInfo);
+router.get("/getUser", protect, getUserInfo);
 
 router.post("/upload-image", upload.single("image"), (req, res) => {
   if (!req.file) {
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 require("dotenv").config();
 
@@ -22,6 +25,9 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
 
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
